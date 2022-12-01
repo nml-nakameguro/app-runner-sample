3a5cc456b88323e92438fafa65fa9aeb269b1107
wangying <ying-wang@mail.nissan.co.jp>
2022-12-01 15:20:53 +0900

    add env file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   myEnv: process.env.NEXT_PUBLIC_MY_ENV,
-  // }
+  env: {
+    myEnv: process.env.TEST_MY_ENV,
+  }
 }
 
 module.exports = nextConfig
